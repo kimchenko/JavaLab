558dfafbb60b9569a03690b2f18f5e793f80cc60
kimchenko <128905075+kimchenko@users.noreply.github.com>
2024-01-23 16:07:47 +0300

    Add files via upload
    #JavaLab 3
    Вспомнить все.
    По введенному номеру дня недели вывести название дня недели, используя оператор множественного выбора (switch).
    Нумерация дней недели начинается с 1 – понедельник, 2 — вторник и т. д.

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+package com.company;
+
+// "Алфавит"
+// Вывести в консоль все буквы от A до Z с помощью цикла. Использовать тип данных char.
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
